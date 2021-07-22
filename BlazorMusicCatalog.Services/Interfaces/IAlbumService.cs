277a13eae72b6198850742ff187a2098aa7a5bb0
aratar79 <aratar79@gmail.com>
2021-07-22 06:36:19 +0200

    create the skeleton of the service layer

---
+++ BlazorMusicCatalog.Services/Interfaces/IAlbumService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorMusicCatalog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Services.Interfaces
+{
+    public interface IAlbumService
+    {
+        Task<IEnumerable<Album>> GetAlbums();
+        Task<Album> GetAlbumDetails(int id);
+        Task<bool> InsertAlbum(Album album);
+        Task<bool> UpdateAlbum(Album album);
+        Task<bool> DeleteAlbum(Album album);
+
+
+    }
+}
---
+++ BlazorMusicCatalog.Services/classes/AlbumService.cs
@@ -0,0 +1,45 @@
+﻿using BlazorMusicCatalog.Data;
+using BlazorMusicCatalog.Models;
+using BlazorMusicCatalog.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Services.classes
+{
+    public class AlbumService : IAlbumService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AlbumService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<bool> DeleteAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Album> GetAlbumDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Album>> GetAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
